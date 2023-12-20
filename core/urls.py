b1ebf8ba8e434ef981f12e9a645db4fc3dee0a44
AlexKasema <lexxmulwa@gmail.com>
2023-12-20 19:23:45 +0300

    Products and Vendors list view Implemented

--- core/urls.py
+++ core/urls.py
@@ -7,6 +7,15 @@ app_name = 'core'
 urlpatterns = [
     path('', views.index, name="index"),
 
+    #! products view
+    path('products', views.product_list_view, name="products"),
+    path('product/<pid>', views.product_details_view, name="product"),
+
     #! category
     path('fruits', views.category_product_fruits_view, name="fruits"),
+    path('vegetables', views.category_product_vegetables_view, name="vegetables"),
+
+    #! vendor
+    path('vendors', views.vendors_list_view, name="vendors"),
+    path('vendor/<vid>', views.vendor_details_view, name="vendor"),
 ]
--- core/views.py
+++ core/views.py
@@ -17,9 +17,59 @@ def index(request):
     }
     return  render(request, 'core/index.html', context)
 
+def product_list_view(request):
+
+    products = Product.objects.filter(product_status = "published", featured=True)
+    vegetables = Product.objects.filter(category__title = "Vegetables")
+    fruits = Product.objects.filter(category__title = "Fruits")
+
+    context = {
+        'products': products, 'vegetables': vegetables, 'fruits': fruits
+    }
+    return render(request, 'core/product_list.html', context)
+
+def product_details_view(request, pid):
+
+    product = Product.objects.get(pid=pid)
+    vegetables = Product.objects.filter(category__title = "Vegetables")
+    fruits = Product.objects.filter(category__title = "Fruits")
+
+    context = {
+        'product': product, 'fruits': fruits, 'vegetables': vegetables
+    }
+
+    return render(request, 'core/product_details.html', context)
+
 def category_product_fruits_view(request):
 
     fruits = Product.objects.filter(category__title = "Fruits")
 
     context = {'fruits': fruits}
     return render(request, 'core/category_fruits_list.html', context)
+
+def category_product_vegetables_view(request):
+
+    vegetables = Product.objects.filter(category__title = "Vegetables")
+
+    context = {'vegetables': vegetables}
+    return render(request, 'core/category_vegetables_list.html', context)
+
+def vendors_list_view(request):
+
+    vendors = Vendor.objects.all()
+
+    context = {
+        'vendors': vendors
+    }
+    return render(request, 'core/vendors_list.html', context)
+
+def vendor_details_view(request, vid):
+
+    vendor = Vendor.objects.get(vid=vid)
+    products = Product.objects.filter(vendor=vendor, product_status = "published")
+
+    context = {
+        'vendor': vendor, 'products': products
+    }
+    return render(request, 'core/vendor_details.html', context)
+
